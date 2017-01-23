74362069dc19b463eed25a74b7e44a21ea1cfb5d
Edgar Pino <me@edgarpino.com>
2017-01-23 13:01:52 -0600

    Added method to create email. Fixed bugs with function.

---
+++ handler.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let api_key = process.env.MAILGUN_API_KEY,
+    domain = process.env.MAILGUN_API_DOMAIN,
+    mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+
+function processData(event) {
+
+  let data = {
+    from: event.body.name + " <" + event.body.from  + ">",
+    to: process.env.MAILGUN_API_EMAIL_TO,
+    subject: process.env.MAILGUN_API_EMAIL_SUBJECT || 'Message From Site',
+    text: event.body.message || ''
+  };
+
+
+  return data;
+
+}
+
+module.exports.hello = (event, context, callback) => {
+
+  let data = processData(event);
+
+  mailgun.messages().send(data, function (error, body) {
+    
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+          message: event.body  
+      }),
+    };
+
+    callback(null, response);
+    
+  });
+  
+  
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
