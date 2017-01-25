fd1c431acead14e5bd2faed569e16b92c7c90cd0
Edgar Pino <me@edgarpino.com>
2017-01-24 22:15:32 -0600

    added cors

--- handler.js
+++ handler.js
@@ -33,6 +33,9 @@ module.exports.hello = (event, context, callback) => {
     
     const response = {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+      },
       body: JSON.stringify({
           message: body
       }),
